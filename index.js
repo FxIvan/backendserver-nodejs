b811f271f05e747fb837e089808849feb59e6b87
Ivan Ariel Almendra <almendraivan210814@gmail.com>
2022-10-02 20:48:08 -0300

    agregando puerto 443

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const app = require('./app')
 
 async function Main(){
-    await app.listen('8000')
-    console.log('Esta escuchando por el puerto 8000')
+    await app.listen('443')
+    console.log('Esta escuchando por el puerto 443')
 }
 
 Main()
